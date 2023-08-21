6176ffbf586c3a7525a864863fa70e33bf84d889
Nymul Islam Moon <towkir19997islam@gmail.com>
2023-08-21 16:55:41 +0600

    initial commit

---
+++ alecadd-plugin.php
@@ -0,0 +1,11 @@
+<?php
+    /**
+     * Plugin Name: Plugin Dev Part 2
+     * Description: This is the ultimate WordPress plugin development
+     * Plugin URL: http://localhost:8888/webapick/plugin
+     * Author: Nymul Isam Moon
+     * Author URI: https://nymul-islam-moon.github.io/
+     * version: 0.0.1
+     * License: GPLv2 or later
+     * Licnse URL: https:www.gnu.org/licenses/gpl-2.0.html
+     */

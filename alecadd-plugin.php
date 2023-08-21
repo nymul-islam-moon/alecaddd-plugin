f7d035aa66edb320a4b33673ad4cd350557e170d
Nymul Islam Moon <towkir19997islam@gmail.com>
2023-08-21 18:08:47 +0600

    Part 3 Video
    11.00 minute from office

--- alecadd-plugin.php
+++ alecadd-plugin.php
@@ -9,3 +9,49 @@
      * License: GPLv2 or later
      * Licnse URL: https:www.gnu.org/licenses/gpl-2.0.html
      */
+
+//    if (!defined('ABSPATH')) {
+//        die;
+//    }
+
+    # OR You can use
+    defined('ABSPATH') or die('Hay, You can not access the area');
+
+     # Or can use
+//    if(!function_exists('add_action')) {
+//        echo 'hay you do not have the access to the area!';
+//        exit;
+//    }
+
+    class alecaddPlugin
+    {
+        # methods
+
+        /**
+         * Constructor for the class
+         */
+        public function __construct()
+        {
+
+        }
+
+        public function method1()
+        {
+
+        }
+
+        public function method2()
+        {
+
+        }
+    }
+
+    if (class_exists('alecaddPlugin')){
+        $alecadddPlugin = new alecaddPlugin();
+    }
+
+    # Activation
+
+    # Deactivating
+
+    # Uninstall

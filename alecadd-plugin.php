10697f636d47089ebdad5bd0b885554db36d0e8f
Nymul Islam Moon <nymulislam.webappick@gmail.com>
2024-01-19 12:17:09 +0600

    Part-4 done

--- alecadd-plugin.php
+++ alecadd-plugin.php
@@ -17,26 +17,31 @@
         /**
          * Constructor for the class
          */
-        public function __construct()
-        {
-
+        public function __construct() {
+            add_action( 'init', array( $this, 'custom_post_type' ) );
         }
 
-        public function activate()
-        {
+        public function activate() {
             // generated a CPT
+            $this->custom_post_type();
             // flush rewrite rules
+            flush_rewrite_rules();
         }
 
-        public function deactivate()
-        {
+        public function deactivate() {
             // flush rewrite rules
+            flush_rewrite_rules();
         }
 
         public function uninstall() {
             // delete CPT
             // delete all the plugin data from the DB
         }
+
+        function custom_post_type() {
+            register_post_type( 'book', [ 'public' => true , 'label' => 'Books' ] );
+        }
+
     }
 
     if ( class_exists( 'AlecadddPlugin' ) ) {
